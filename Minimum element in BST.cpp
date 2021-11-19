9b343e230135a5050aaed2412697cdc12142ba30
Ahmad Mustaffa <49057200+Mustaffa96@users.noreply.github.com>
2021-11-19 16:24:48 +0800

    Create Minimum element in BST.cpp

---
+++ Minimum element in BST.cpp
@@ -0,0 +1,52 @@
+/*
+Given a Binary Search Tree. The task is to find the minimum element in this given BST.
+
+Example 1:
+
+Input:
+           5
+         /    \
+        4      6
+       /        \
+      3          7
+     /
+    1
+Output: 1
+Example 2:
+
+Input:
+             9
+              \
+               10
+                \
+                 11
+Output: 9
+Your Task:
+The task is to complete the function minValue() which takes root as the argument and returns the minimum element of BST. If the tree is empty, there is no minimum elemnt, so retutn -1 in that case.
+
+Expected Time Complexity: O(Height of the BST)
+Expected Auxiliary Space: O(Height of the BST).
+
+Constraints:
+1 <= N <= 104
+*/
+
+
+
+
+int minValue(struct Node *root) {
+    if (root == NULL)
+      return minValue;
+ 
+    // Return minimum of 3 values:
+    // 1) Root's data 2) Max in Left Subtree
+    // 3) Max in right subtree
+    int res = root->data;
+    int lres = minValue(root->left);
+    int rres = minValue(root->right);
+    if (lres < res)
+      res = lres;
+    if (rres < res)
+      res = rres;
+    return res;
+}
