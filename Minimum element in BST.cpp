681bc366bd366f2108762447512e0e332a9146bf
Ahmad Mustaffa <49057200+Mustaffa96@users.noreply.github.com>
2021-11-19 16:29:17 +0800

    Update Minimum element in BST.cpp

--- Minimum element in BST.cpp
+++ Minimum element in BST.cpp
@@ -50,3 +50,61 @@ int minValue(struct Node *root) {
       res = rres;
     return res;
 }
+
+
+//Fullcode
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+ 
+// A tree node
+class Node {
+public:
+    int data;
+    Node *left, *right;
+ 
+    /* Constructor that allocates a new
+    node with the given data and NULL
+    left and right pointers. */
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+int main()
+{
+    Node* NewRoot = NULL;
+    Node* root = new Node(2);
+    root->left = new Node(7);
+    root->right = new Node(5);
+    root->left->right = new Node(6);
+    root->left->right->left = new Node(1);
+    root->left->right->right = new Node(11);
+    root->right->right = new Node(9);
+    root->right->right->left = new Node(4);
+ 
+    // Function call
+    cout << "Minimum element is " << findMin(root) << endl;
+ 
+    return 0;
+}
+int findMin(struct Node* root)
+{
+    // Base case
+    if (root == NULL)
+      return INT_MAX;
+ 
+    // Return minimum of 3 values:
+    // 1) Root's data 2) Max in Left Subtree
+    // 3) Max in right subtree
+    int res = root->data;
+    int lres = findMin(root->left);
+    int rres = findMin(root->right);
+    if (lres < res)
+      res = lres;
+    if (rres < res)
+      res = rres;
+    return res;
+}
