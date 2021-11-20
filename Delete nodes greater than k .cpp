938585d4845b5ffabce5f03a00264d62de7e1a95
Ahmad Mustaffa <49057200+Mustaffa96@users.noreply.github.com>
2021-11-20 17:35:42 +0800

    Update Delete nodes greater than k .cpp

--- Delete nodes greater than k .cpp
+++ Delete nodes greater than k .cpp
@@ -23,94 +23,30 @@ Constraints:
 1 <= x <= N
 */
 
-#include <bits/stdc++.h>
-using namespace std;
-
-class Node{
-	public:             
-	int data;           //value
-	Node *left;    //pointer to left child
-	Node *right;   //pointer to right child
-};
-
-// creating new node
-Node* newnode(int data)  
-{ 
-	Node* node = (Node*)malloc(sizeof(Node)); 
-	node->data = data; 
-	node->left = NULL; 
-	node->right = NULL; 
-	return(node); 
-}
-
-Node* buildTree(Node* root,int key){
+class Solution{
+  public:
+  
+  Node* buildTree(Node* root,int k){
 	if(!root)
 		return NULL;
-	if(root->data==key){
+	if(root->data==k){
 		return root->left; //only left subtree not deleted
 	}
-	else if(root->data>key)
-		return buildTree(root->left,key); //recur for left subtree
+	else if(root->data>k)
+		return buildTree(root->left,k); //recur for left subtree
 	else{
 		//root not deleted
 		//left subtree not deleted
 		//buld right subtree
-		root->right=buildTree(root->right,key); 
+		root->right=buildTree(root->right,k); 
 		return root;
 	}
 }
-
-Node* deleteNode(Node* root, int key)
-{
-	root=buildTree(root,key);
+  
+    /*The function should return the root of the modified tree*/
+    Node* deleteNode(Node* root, int k)
+    {
+	root=buildTree(root,k);
 	return root; 
-}
-
-void levelwisedisplay(Node *root)
-{
-	//to display the tree level wise
-	queue<Node*> q;
-
-	Node* temp;
-	int count=0;
-	q.push(root);
-	q.push(NULL);
-
-	while(!q.empty()){
-		temp=q.front();
-		q.pop();
-
-		if(temp==NULL){
-			if(!q.empty())
-				q.push(NULL);
-			cout<<"\nend of level "<<count++<<endl;
-		}
-		else{
-			cout<<temp->data<<" ";
-			if(temp->left)
-				q.push(temp->left);
-			if(temp->right)
-				q.push(temp->right);
-		}
-	}
-	return ;
-}
-
-int main(){
-	cout<<"tree is built as per example\n";
-
-	Node *root=newnode(4); 
-	root->left= newnode(1); 
-	root->right= newnode(9); 
-
-
-	printf("Displaying level wise\n");
-	levelwisedisplay(root);
-
-	root=deleteNode(root,2);//as per example
-
-	printf("after deleting nodes greater or equal to 2\n");
-	levelwisedisplay(root);
-
-	return 0;
-}
+    }
+};
