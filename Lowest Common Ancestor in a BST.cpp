5cd54265d15cdc871f686d30c732cd8fd84e671d
Ahmad Mustaffa <49057200+Mustaffa96@users.noreply.github.com>
2021-11-20 17:37:48 +0800

    Update Lowest Common Ancestor in a BST.cpp

--- Lowest Common Ancestor in a BST.cpp
+++ Lowest Common Ancestor in a BST.cpp
@@ -31,70 +31,23 @@ Constraints:
 1 <= N <= 104
 */
 
-// A recursive CPP program to find
-// LCA of two nodes n1 and n2.
-#include <bits/stdc++.h>
-using namespace std;
-
-class node
+Node* LCA(Node *root, int n1, int n2)
 {
-	public:
-	int data;
-	node* left, *right;
-};
-
-/* Function to find LCA of n1 and n2.
-The function assumes that both
-n1 and n2 are present in BST */
-node *lca(node* root, int n1, int n2)
-{
-	if (root == NULL) return NULL;
-
-	// If both n1 and n2 are smaller
-	// than root, then LCA lies in left
-	if (root->data > n1 && root->data > n2)
-		return lca(root->left, n1, n2);
-
-	// If both n1 and n2 are greater than
-	// root, then LCA lies in right
-	if (root->data < n1 && root->data < n2)
-		return lca(root->right, n1, n2);
-
-	return root;
+  if(root==NULL){
+      return root;
+  }
+  if(root->data==n1 || root->data==n2){
+      return root;
+  }
+  Node* lca1=LCA(root->left,n1,n2);
+  Node* lca2=LCA(root->right,n1,n2);
+  if(lca1!=NULL && lca2!=NULL){
+      return root;
+  }
+  if(lca1!=NULL){
+      return lca1;
+  }
+  else{
+      return lca2;
+  }
 }
-
-/* Helper function that allocates
-a new node with the given data.*/
-node* newNode(int data)
-{
-	node* Node = new node();
-	Node->data = data;
-	Node->left = Node->right = NULL;
-	return(Node);
-}
-
-/* Driver code*/
-int main()
-{
-	// Let us construct the BST
-	// shown in the above figure
-	node *root = newNode(2);
-	root->left = newNode(1);
-	root->right = newNode(3);
-
-
-	int n1 = 10, n2 = 14;
-	node *t = lca(root, n1, n2);
-	cout << "LCA of " << n1 << " and " << n2 << " is " << t->data<<endl;
-
-	n1 = 14, n2 = 8;
-	t = lca(root, n1, n2);
-	cout<<"LCA of " << n1 << " and " << n2 << " is " << t->data << endl;
-
-	n1 = 1, n2 = 3;
-	t = lca(root, n1, n2);
-	cout << "LCA of " << n1 << " and " << n2 << " is " << t->data << endl;
-	return 0;
-}
-
-
